80a16cf2a52d87e7b8c7d398c8da0012b8bb55c6
Rafaella <gomesrafaella479@gmail.com>
2024-03-20 16:06:20 +0000

    add controllers, use cases and ci

---
+++ src/http/controllers/check-ins/validate.ts
@@ -0,0 +1,19 @@
+import { z } from 'zod'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { makeValidateCheckInUseCase } from '@/use-cases/factories/make-validate-check-in-use-case'
+
+export async function validate(request: FastifyRequest, reply: FastifyReply) {
+  const validateCheckInParamsBodySchema = z.object({
+    checkInId: z.string().uuid(),
+  })
+
+  const { checkInId } = validateCheckInParamsBodySchema.parse(request.params)
+
+  const validateCheckInUseCase = makeValidateCheckInUseCase()
+
+  await validateCheckInUseCase.execute({
+    checkInId,
+  })
+
+  return reply.status(204).send()
+}
